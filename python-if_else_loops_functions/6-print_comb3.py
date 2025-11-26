fdce7d6d61524973db9abb9eb73cf75948a95776
Sabir <12734@holbertonstudents.com>
2025-11-26 09:19:44 +0000

    Created a new file

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+for i in range(100):
+    for x in range(i+1, 10):
+        if int(str("{}{}".format(i, x))) != 89:
+            print('{}{}'.format(i, x), end=", ")
+        else:
+            print('89', end='')
