0efbe2abf6d09d2e0386bcc401b14042a60efb52
Sabir <12734@holbertonstudents.com>
2025-11-26 10:07:57 +0000

    Created a new file

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(string, n):
+    if len(n) >= n > 0:
+        lss = list(string)
+        lss.pop(n)
+        return ''.join(lss)
+    else:
+        return string
