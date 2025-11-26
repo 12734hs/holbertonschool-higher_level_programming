08667721917b8a630418db79cafcbb283b48936b
Sabir <12734@holbertonstudents.com>
2025-11-26 10:10:43 +0000

    Updated

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(string, n):
-    if len(string) >= n > 0:
+    if len(string) >= n >= 0:
         lss = list(string)
         lss.pop(n)
         return ''.join(lss)
