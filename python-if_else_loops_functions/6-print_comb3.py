dfa50cfae4102669cc6de8b30d1f1385f31baa95
Sabir <12734@holbertonstudents.com>
2025-11-26 09:26:39 +0000

    Updated

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(100):
         if int(str("{}{}".format(i, x))) != 89:
             print('{}{}'.format(i, x), end=", ")
         else:
-            print('89', end=' ')
+            print('89')
