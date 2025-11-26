7410f63045955d4d6732d7422bf8f1e60f0c1776
Sabir <12734@holbertonstudents.com>
2025-11-26 08:23:58 +0000

    Updated

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,6 @@ def print_last_digit(number):
     if number!= 0:
         result = int(str(number)[-1])
         print("{}".format(result), end='')
+        return result
     else:
         print("0")
