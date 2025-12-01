c5c0390a79baf1ee31f8a9529d6628a8cd0fe82c
Sabir <12734@holbertonstudents.com>
2025-12-01 11:47:34 +0000

    Updated a file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,17 @@ import sys
 lst = sys.argv
 lst.pop(0)
 
-if len(lst) != 0:
-    if len(lst) != 1:
-        print("{} arguments:".format(len(lst)))
-        for i in range(0, len(lst)):
-            print("{}: {}".format(i + 1, lst[i]))
+def func():
+    if len(lst) != 0:
+        if len(lst) != 1:
+            print("{} arguments:".format(len(lst)))
+            for i in range(0, len(lst)):
+                print("{}: {}".format(i + 1, lst[i]))
+        else:
+            print("{} argument:".format(len(lst)))
+            print("{}: {}".format(len(lst), lst[0]))
     else:
-        print("{} argument:".format(len(lst)))
-        print("{}: {}".format(len(lst), lst[0]))
-else:
-    print("0 arguments.")
+        print("0 arguments.")
+
+if __name__ == "__main__":
+    func()
