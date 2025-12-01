dc7c7f82adf46506e67d8417b2b04cc758ddfdf9
Sabir <12734@holbertonstudents.com>
2025-12-01 11:44:50 +0000

    Created a new file

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+lst = sys.argv
+lst.pop(0)
+
+if len(lst) != 0:
+    if len(lst) != 1:
+        print("{} arguments:".format(len(lst)))
+        for i in range(0, len(lst)):
+            print("{}: {}".format(i + 1, lst[i]))
+    else:
+        print("{} argument:".format(len(lst)))
+        print("{}: {}".format(len(lst), lst[0]))
+else:
+    print("0 arguments.")
