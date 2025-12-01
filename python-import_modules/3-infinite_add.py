97e46ace4656f876c85c9f77230df8bfc2319411
Sabir <12734@holbertonstudents.com>
2025-12-01 11:55:53 +0000

    Updated a file name

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,10 +4,10 @@ import sys
 lst = sys.argv
 lst.pop(0)
 
-accumulator = 0
+acumulator = 0
 
 for i in lst:
-    accumulator += int(i)
+    acumulator += int(i)
 
 if __name__ == "__main__":
-    print(accumulator)
+    print(acumulator)
