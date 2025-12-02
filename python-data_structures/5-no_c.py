34c339535fbdf0cbc56b9802ca40976827623035
Sabir <12734@holbertonstudents.com>
2025-12-02 09:08:39 +0000

    Updateed

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     lst_letters = list(my_string)
     for i in my_string:
         if i == 'c' or i == "C":
-            index_of_lst = lst_lettters.index(i)
+            index_of_lst = lst_letters.index(i)
             lst_letters.pop(index_of_lst)
 
     return ''.join(lst_letters)
