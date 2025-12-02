7c3dc2dfa90ba23f1ef284d46885d750c36fd36b
Sabir <12734@holbertonstudents.com>
2025-12-02 09:15:57 +0000

    Updated

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for lists in matrix:
         for i in lists:
-            print('{:d}'.format(i), end='')
+            print('{:d}'.format(i), end=' ')
         print('')
