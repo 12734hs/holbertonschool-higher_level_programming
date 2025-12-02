1cf3d677ebea7e5b612472f4b8dd7746fc0ca3e6
Sabir <12734@holbertonstudents.com>
2025-12-02 11:20:23 +0000

    Updated

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     if not new_lst:
         return []
     else:
-        for i in lst:
+        for i in new_list:
             if i % 2 == 0:
                 new_lst[my_list.index(i)] = True
             else:
