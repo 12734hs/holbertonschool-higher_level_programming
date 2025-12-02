ef4d89752af434f6941290944f5f359b6b664320
Sabir <12734@holbertonstudents.com>
2025-12-02 10:11:58 +0000

    Updated

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -17,5 +17,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return new_tuple
     elif len(tuple_a) < 1 and len(tuple_b) > 1:
         return tuple_b
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        new_tuple = (tuple_a[0] + tuple_b[0],)
+        return new_tuple
     else:
         return ()
