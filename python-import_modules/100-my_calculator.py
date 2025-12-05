45a998cc82bb56a1ce9610b964fbd0655d9146f7
Sabir <12734@holbertonstudents.com>
2025-12-05 08:08:47 +0000

    Updated a file

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,34 +5,37 @@ list_of_arguments = sys.argv
 list_of_arguments.pop(0)
 list_of_operators = ['+', '-', '*', '/']
 
-
-if len(list_of_arguments) == 3:
-    a = int(list_of_arguments[0])
-    b = int(list_of_arguments[2])
-    operator = list_of_arguments[1]
+def print_result():
+    if len(list_of_arguments) == 3:
+        a = int(list_of_arguments[0])
+        b = int(list_of_arguments[2])
+        operator = list_of_arguments[1]
     
-    if operator == '+':
-        result = add(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result))
-        print(0)
+        if operator == '+':
+            result = add(a, b)
+            print('{} {} {} = {}'.format(a, operator, b, result))
+            print(0)
         
-    elif operator == '-':
-        result = sub(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result))
-        print(0)
+        elif operator == '-':
+            result = sub(a, b)
+            print('{} {} {} = {}'.format(a, operator, b, result))
+            print(0)
 
-    elif operator == '*':
-        result = mul(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result))
-        print(0)
+        elif operator == '*':
+            result = mul(a, b)
+            print('{} {} {} = {}'.format(a, operator, b, result))
+            print(0)
 
-    elif operator == '/':
-        result = div(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result))
-        print(0)
+        elif operator == '/':
+            result = div(a, b)
+            print('{} {} {} = {}'.format(a, operator, b, result))
+            print(0)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            print(1)
     else:
-        print('Unknown operator. Available operators: +, -, * and /')
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         print(1)
-else:
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    print(1)
+
+if __name__ == "__main__":
+    print_result()
