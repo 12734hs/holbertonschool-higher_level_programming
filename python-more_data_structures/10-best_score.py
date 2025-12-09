ca4f00c982106159873a7e4eab9ca03c105ae16a
Sabir <12734@holbertonstudents.com>
2025-12-09 10:25:11 +0000

    Created a file

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        key = ''
+        grades = list(a_dictionary.values())
+        max_grade = max(grades)
+        for i in a_dictionary.keys():
+            if a_dictionary[i] == max_grade:
+                key = i
+            else:
+                pass
+    print(key)
