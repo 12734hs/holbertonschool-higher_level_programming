38db3b4f891fca02495e8be4d8d7677bb6d22410
Sabir <12734@holbertonstudents.com>
2025-12-09 14:30:59 +0000

    Updated

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman_letter = {"C": 100, 'L': 50, "X": 10, "V": 5, "I": 1}
-    if type(roman_string) == str and roman_string is not None:
+    if isinstance(roman_string, str) and not isinstance(roman_string, type(None)):
         accumulator = 0
         for index, roman in enumerate(roman_string):
+            now_number = roman_letter[roman_string[index]]
             if index == 0:
                 accumulator += roman_letter[roman]
-            elif roman_letter[roman_string[index-1]] < roman_letter[roman_string[index]]:
-                accumulator += roman_letter[roman_string[index]] - 2 * roman_letter[roman_string[index-1]]
+            elif roman_letter[roman_string[index-1]] < now_number:
+                accumulator += now_number - 2 * roman_letter[roman_string[index-1]]
             else:
                 accumulator += roman_letter[roman]
         return accumulator
