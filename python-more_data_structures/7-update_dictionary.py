f9c26212520d6becba210f50748d36461692587c
Sabir <12734@holbertonstudents.com>
2025-12-09 09:55:18 +0000

    Updated a file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_list = a_dictionary.copy()
-    new_list[key] = value
-    return new_list
+    a_dictionary[key] = value
+    return a_dictionary
