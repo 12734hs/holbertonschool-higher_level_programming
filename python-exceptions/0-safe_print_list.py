6441b045feb565f79fdbb80ca642d0e763fe0b3c
Sabir <12734@holbertonstudents.com>
2025-12-10 11:14:09 +0000

    Updated a file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,12 +5,12 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
         print()
-        return x 
+        return x
     except IndexError:
         x = 0
         for item in my_list:
-          x += 1
+            x += 1
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
         print()
-        return x 
+        return x
