5d1cd52dfe3225b57b0f268c2aac08160163e745
Sabir <12734@holbertonstudents.com>
2025-12-10 11:54:45 +0000

    Updated

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,6 +3,6 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except:
-        print("{Inside result: }".format(None))
+        print("Inside result: {}".format(None))
     finally:
-        print("{Inside result: }".format(result))
+        print("Inside result: {}".format(result))
