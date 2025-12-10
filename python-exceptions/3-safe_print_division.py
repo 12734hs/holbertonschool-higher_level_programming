b70f554596843d6f377e5d6155e0af811ee1731a
Sabir <12734@holbertonstudents.com>
2025-12-10 12:01:35 +0000

    Updated

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except DivisionByZeroError:
-        result = None
+    except:
+        pass
     finally:
         print("Inside result: {}".format(result))
