2c3dd740a82814ec02d357fbc8651b660d56a767
12734hs <12734@holbertonstudents.com>
2025-12-11 15:54:58 +0400

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        a = my_list[x]
-        for i in range(0, x):
+    for i in range(0, x):
+        try:
             print("{:d}".format(my_list[i]), end="")
-        print()
-        return x
-    except ValueError:
-        pass
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return x-1
