eb7f990c656827a35c4c904be54af08dbb1a0dea
12734hs <12734@holbertonstudents.com>
2025-12-11 16:07:34 +0400

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             accumulator += 1
         except (ValueError, TypeError):
             pass
-    
     print(final_sentence)
     return accumulator
