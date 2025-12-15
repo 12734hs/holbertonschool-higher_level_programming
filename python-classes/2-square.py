c100f2a164f0ac72705bee7199be654d35596165
Sabir <12734@holbertonstudents.com>
2025-12-15 12:41:50 +0000

    Updated

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,12 +4,15 @@
 
 class Square:
     """This class was created for make square guy"""
-    def __init__(self, size=0):
-        check = isinstance(size, int)
-        check2 = size > 0
-        if check == True and check2 == True:
-            self.__size = size
-        elif check != True and check2 == True:
-            raise TypeError("size must be an integer")
-        elif check == True and check2 != True:
-            raise ValueError ("size must be >= 0")
+    def __init__(self, size=None):
+        if size == None:
+            pass
+        else:
+            check = isinstance(size, int)
+            check2 = size > 0
+            if check == True and check2 == True:
+                self.__size = size
+            elif check != True and check2 == True:
+                raise TypeError("size must be an integer")
+            elif check == True and check2 != True:
+                raise ValueError ("size must be >= 0")
