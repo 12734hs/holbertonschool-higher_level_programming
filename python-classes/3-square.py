f85248d56519aa23576ca1fac35eb539cc308ff3
Sabir <12734@holbertonstudents.com>
2025-12-15 12:57:57 +0000

    Created a new file

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This class was created for make square guy"""
+
+
+class Square:
+    """This class was created for make square guy"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size > 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+    
+    def square(self):
+        return self.__size * self.__size
