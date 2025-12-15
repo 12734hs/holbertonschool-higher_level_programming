208a6efe7925cd615b51e0b1d430a5e4d4fb652a
Sabir <12734@holbertonstudents.com>
2025-12-15 11:24:56 +0000

    Updated

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This class was created for make square guy"""
+
+
+class Square:
+    """This class was created for make square guy"""
+    def __init__(self, size):
+        self.__size = size
