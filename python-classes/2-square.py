c5db01397e355a10d4e76f59d0c8b63389d90d6c
Sabir <12734@holbertonstudents.com>
2025-12-15 12:37:52 +0000

    Created a new file

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This class was created for make square guy"""
+
+
+class Square:
+    """This class was created for make square guy"""
+    def __init__(self, size):
+        check = isinstance(size, int)
+        check2 = size > 0
+        if check == True and check2 == True:
+            self.__size = size
+        elif check != True and check2 == True:
+            raise TypeError("size must be an integer")
+        elif check == True and check2 != True:
+            raise ValueError ("size must be >= 0")
