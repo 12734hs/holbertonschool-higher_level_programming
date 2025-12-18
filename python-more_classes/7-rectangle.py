5d841f97d8dc03f29e1945cf0cc32296f66b3801
Sabir <12734@holbertonstudents.com>
2025-12-18 09:41:18 +0000

    DUpdated

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     number_of_instances = 0
 
     print_symbol = "#"
-    
+ 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
         if isinstance(width, int):
