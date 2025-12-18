f936ea6ef06d1e329615e78bd000b5440884a21a
Sabir <12734@holbertonstudents.com>
2025-12-18 09:23:25 +0000

    Created a new file

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Everyone wants to rule the world"""
+
+
+class Rectangle:
+    number_of_instances = 0
+    """I'm your father luke"""
+    def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
+        if isinstance(width, int):
+            if width >= 0:
+                self.__width = width
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+        if isinstance(height, int):
+            if height >= 0:
+                self.__height = height
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        return self.__height ** self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__height * 2) + (self.__width * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        else:
+            sentence = ""
+            for i in range(0, self.__height):
+                sentence += self.__width * "#" + "\n"
+            return sentence[:-1]
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
