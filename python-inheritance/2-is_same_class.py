0d7d9d78bf9752011b5b4e9d0fa34d54028b44c7
Sabir <12734@holbertonstudents.com>
2025-12-22 10:11:49 +0000

    Created a file

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is abput inheritance in oop python"""
+
+
+def is_same_class(obj, a_class):
+    """this is method for check is the object is classes or not"""
+    return isinstance(obj, a_class)
