40c7b7e8d2df42bc8fa291d14ae3e7560e6d4f89
Sabir <12734@holbertonstudents.com>
2025-12-22 09:55:46 +0000

    Created a new file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is module about inheritance"""
+
+
+class MyList(list):
+    """This is a class about lines"""
+
+    def print_sorted(self):
+        print(sorted(self))
