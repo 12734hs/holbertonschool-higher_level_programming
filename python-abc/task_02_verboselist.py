09e68882d9f0f465c80d990c075862a3670a60df
12734hs <12734@holbertonstudents.com>
2025-12-25 13:04:02 +0400

    Update task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+class VerboseList(list):
+    def append(self, x):
+        super().append(x)
+        print('Added {} to the list.'.format(x))
+
+    def extend(self, x):
+        super().extend(x)
+        print('Extended the list with {} items.'.format(len(x)))
+
+    def remove(self, x):
+        super().remove(x)
+        print('Removed {} from the list.'.format(x))
+
+    def pop(self, x=-1):
+        super().pop(x)
+        print('Popped {} from the list.'.format(self[x]))
+
+
+lst = VerboseList()
+lst.append('a')
+lst.extend(['b', 'c', 'd'])
+lst.remove('b')
+lst.pop(1)
