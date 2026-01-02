c48be4539e864f6581854f34ca8e719931d544a6
Sabir <12734@holbertonstudents.com>
2026-01-02 20:31:51 +0000

    updated

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,18 +10,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs:
-            if len(attrs) >= 0:
-                new_dict = {}
-                for i in attrs:
-                    if i in self.__dict__.keys():
-                        new_dict[i] = self.__dict__[i]
-                    else:
-                        pass
-                else:
-                    pass
-            else:
-                pass
-            return new_dict
-        else:
+        if attrs is None:
             return self.__dict__
+        else:
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+
