bfeb29fa95afee46a0cf2720c29eaebab5dd8020
Sabir <12734@holbertonstudents.com>
2026-01-02 20:00:35 +0000

    updated

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """funct"""
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file, indent=4, ensure_ascii=False)
+        json.dump(my_obj, file, ensure_ascii=False)
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """I/O"""
-import sys
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
 
+import sys
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
     try:
-        items = load('add_item.json')
+        lst = load("add_item.json")
     except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save(items, 'add_item.json')
+        lst = []
+    lst.extend(sys.argv[1:])
+    save(lst, "add_item.json")
