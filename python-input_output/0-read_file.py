f5e9395f5dec105bf6f78b4210a8d3dbed002fbe
Sabir <12734@holbertonstudents.com>
2026-01-02 18:09:31 +0000

    Updated

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
-  1 #!/usr/bin/python3
-  2 """PART:I/O"""
-  3 
-  4 
-  5 def read_file(filename=''):
-  6     """funct"""
-  7     with open(filename, 'r') as file:
-  8         return file.read()
+#!/usr/bin/python3
+"""PART:I/O""" 
+
+
+def read_file(filename=''):
+    """funct"""
+    with open(filename, 'r') as file:
+        return file.read()
