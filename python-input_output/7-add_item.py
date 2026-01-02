64bca1273b9ae11a7503d88bef508c2f42a364ac
Sabir <12734@holbertonstudents.com>
2026-01-02 19:42:53 +0000

    updated

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+#!/usrbin/python3
+"""I/O"""
+
 import sys
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
 
-"""Idf/O"""
-
-argm = sys.argv
-null = argm.pop(0)
-
-lst = load_from_json_file("add_item.json")
-save_to_json_file(argm, lst)
+if __name__ == "__main__":
+    lst = load("add_file.json")
+    save(sys.argv[1:], lst)
