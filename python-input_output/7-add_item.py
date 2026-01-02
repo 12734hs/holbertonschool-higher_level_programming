aa1532137b2168645f3a2cd534e4f178d996b9bc
Sabir <12734@holbertonstudents.com>
2026-01-02 19:51:09 +0000

    updated

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ save = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
     try:
-        lst = load("add_file.json")
+        lst = load("add_item.json")
     except FileNotFoundError:
         lst = []
     lst.extend(sys.argv[1:])
-    save(lst, "add_file.json")
+    save(lst, "add_item.json")
