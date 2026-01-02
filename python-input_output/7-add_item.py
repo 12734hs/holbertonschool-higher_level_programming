7731652346dae78de859358e2d9c949122a145ba
Sabir <12734@holbertonstudents.com>
2026-01-02 19:32:02 +0000

    created a new file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,10 @@
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+import sys
+"""I/O"""
+
+argm = sys.argv
+null = argm.pop(0)
+
+lst = load_from_json_file("add_item.json")
+save_to_json_file(argm, lst)
