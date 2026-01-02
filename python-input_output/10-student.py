d0d20b1ed6be8b2610e9304f7e55af771cf3a0fd
Sabir <12734@holbertonstudents.com>
2026-01-02 20:24:17 +0000

    Created a enw file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""PART:I/O"""
+
+
+class Student:
+    """classlolmessipessipenaldo"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            new_dict = {}
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    new_dict[i] = self.__dict__[i]
+                else:
+                    pass
+            else:
+                pass
+            return new_dict
+        else:
+            return self.__dict__
