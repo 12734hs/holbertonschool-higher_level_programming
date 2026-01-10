7ac32730833264df6a70350fa4623569952dae6d
SabSuf <12734@holbertonstudents.com>
2026-01-10 17:58:35 +0400

    Update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,55 +1,95 @@
-#!/usr/bin/python3
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt
 
-users = {}
+
 app = Flask(__name__)
 
+app.config['JWT_SECRET_KEY'] = 'fYLv51a4aDURpIkGdBqUCBBv6rk'
+jwt = JWTManager(app)
+auth = HTTPBasicAuth()
 
-@app.route('/')
-def home():
-    return "Welcome to the Flask API!"
+# Our DATABASE
+users = {
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+}
 
-
-@app.route('/data')
-def data():
-    return jsonify(list(users.keys()))
-
-
-@app.route('/status')
-def status():
-    return "OK"
+# -----------------------------------------------------------
 
 
-@app.route('/users/<username>')
-def return_info(username):
+@auth.verify_password
+def verify_password(username, password):
     user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({'error': "User not found"}), 404
+    if user and check_password_hash(user['password'], password):
+        return username
+    return None
 
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
+@app.route('/basic-protected', methods=['GET'])
+@auth.login_required
+def basic_protected():
+    return "Basic Auth: Access Granted"
 
-    data_dct = request.get_json()
 
-    if not data_dct:
-        return jsonify({'error': 'Invalid JSON'}), 400
+@app.route('/login', methods=['POST'])
+def login():
+    # format JSON: {"username": "user1", "password": "password"}
+    user = request.get_json()
+    if not user or 'username' not in user or 'password' not in user:
+        return jsonify({'error': 'Invalid Credentials'}), 401
 
-    username = data_dct.get('username')
+    username = user.get('username')
+    password = user.get('password')
+    role = users[username]['role']
 
-    if not username:
-        return jsonify({'error': 'Username is required'}), 400
+    if username in users and check_password_hash(users[username]['password'], password):
+        access_token = create_access_token(identity={'username': username, 'role': role})
+        return jsonify(access_token=access_token)
+    return jsonify({'error': 'Wrong data'}), 401
 
-    if username in users:
-        return jsonify({'error': 'Username already exists'}), 409
 
-    users[username] = data_dct
-    message = {'message': 'User added', 'user': data_dct}
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
 
-    return jsonify(message), 201
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
+
+
+@app.route('/jwt-protected', methods=['GET'])
+@jwt_required()
+def protected():
+    return 'JWT Auth: Access Granted'
+
+
+@app.route('/admin-only', methods=["GET"])
+@jwt_required()
+def check_admin():
+    data = get_jwt()
+    if data.get('role') != 'admin':
+        return jsonify({'error': 'Admin access required'}), 403
+
+    return 'Admin Access: Granted'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
