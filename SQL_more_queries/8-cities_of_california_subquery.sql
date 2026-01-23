00c0c07dd7a319cd1fccc57db31f24fc18672122
SabSuf <12734@holbertonstudents.com>
2026-01-23 13:22:15 +0400

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Hello popl
-SELECT name FROM cities WHERE cities.id = (SELECT id FROM states) ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
