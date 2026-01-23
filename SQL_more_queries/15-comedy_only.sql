fd1a90475b130dcdc98b1529a1691ed67c685674
sabir <12734@holbertonstudents.com>
2026-01-23 10:55:35 +0000

    Created a new file

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- Hello I m the pewdiepie
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title ASC;
