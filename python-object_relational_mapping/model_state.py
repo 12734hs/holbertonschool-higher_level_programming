eecfc50143ae864f7de41600d0fd45bbfbaa0f21
SabSuf <12734@holbertonstudents.com>
2026-01-25 01:14:17 +0400

    Create model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sqlalchemy import column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'state'
+    id = column(Integer, primary_key=True, nullable=False)
+    name = column(String(128))
