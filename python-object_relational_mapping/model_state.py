5ba657e46ae5871127bd9d605389373c6b66611e
SabSuf <12734@holbertonstudents.com>
2026-01-25 01:16:35 +0400

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """HIIIIIIIIIIIIII"""
-from sqlalchemy import column, Integer, String
+from sqlalchemy import Сolumn, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'state'
-    id = column(Integer, primary_key=True, nullable=False)
-    name = column(String(128))
+    id = Сolumn(Integer, primary_key=True, nullable=False)
+    name = Сolumn(String(128))
