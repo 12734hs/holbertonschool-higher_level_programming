1ba79c1b0d42d0892b1aa40051a5e06a6a6d01aa
SabSuf <12734@holbertonstudents.com>
2026-01-25 01:41:34 +0400

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,30 @@
+# #!/usr/bin/python3
+# """
+# Lists all State objects from the database hbtn_0e_6_usa
+# """
+# import sys
+# from model_state import Base, State
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+
+# if __name__ == "__main__":
+#     # движок
+#     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+#         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     states = session.query(State).order_by(State.id).all()
+
+#     for state in states:
+#         print("{}: {}".format(state.id, state.name))
+
+#     session.close()
 #!/usr/bin/python3
 """
-Lists all State objects from the database hbtn_0e_6_usa
+Lists all State objects from the database hbtn_0e_6_usa.
 """
 import sys
 from model_state import Base, State
@@ -9,16 +33,23 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # движок
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    # Create the engine to connect to the MySQL server
+    # Credentials are taken from command line arguments
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session
     session = Session()
 
+    # Query all State objects, sorted by id in ascending order
     states = session.query(State).order_by(State.id).all()
 
+    # Display the results
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
+    # Close the session
     session.close()
