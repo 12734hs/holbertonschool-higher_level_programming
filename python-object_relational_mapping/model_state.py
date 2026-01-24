74703297c54bb5aa4d74bb8f9129c331bf8f90f7
SabSuf <12734@holbertonstudents.com>
2026-01-25 01:23:02 +0400

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""YESSSSSSSSSSSSSSSSSSSSSSS"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-"""YESSSSSSSSSSSSSSSSSSSSSSS"""
 
 Base = declarative_base()
 
