9ae447227e48b912ce91c8faf674f55308915165
Sabir <12734@holbertonstudents.com>
2026-01-25 06:09:47 +0000

    Created a enw file

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+# !/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # движок
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    state_which_was_given = sys.argv[4]
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louis = State(name='Louisiana')
+    session.add(louis)
+    session.commit()
+
+    state = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
