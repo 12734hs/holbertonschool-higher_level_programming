ed0c363fed5a1308c8547d3d4048a4f7777c0555
Sabir <12734@holbertonstudents.com>
2026-02-01 22:30:49 +0400

    Updated

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 const massivOfArguments = process.argv;
 const value = Number(massivOfArguments[2]);
 
-if (Number.isNaN(value)){
+if (Number.isNaN(value)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${massivOfArguments[2]}`)
+  console.log(`My number: ${massivOfArguments[2]}`);
 }
