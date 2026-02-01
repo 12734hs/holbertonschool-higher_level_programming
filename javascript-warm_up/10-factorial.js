77113ca4cd4effec7bc37db940f0e49c6351bd62
Sabir <12734@holbertonstudents.com>
2026-02-01 23:11:11 +0400

    Updated

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,14 +2,13 @@
 
 const number = Number(process.argv[2]);
 
-function factorial(num) {
-  if (Number.isNaN(num) || num < 0) return 'Not a number';
+function factorial (num) {
+  if (Number.isNaN(num) || num < 0) return '1';
 
   let result = 1;
   for (let i = 1; i <= num; i++) {
     result *= i; 
   }
-
   return result;
 }
 console.log(factorial(number));
