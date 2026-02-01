b95b6e8ea21cfc7c119f6bdd1996638316611937
Sabir <12734@holbertonstudents.com>
2026-02-01 22:49:52 +0400

    Updated

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const number = Number(process.argv[2]);
 if (Number.isNaN(number)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let i = 0; i < number; i++) {
     console.log('X'.repeat(number));
