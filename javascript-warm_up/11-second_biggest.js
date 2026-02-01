82e0ce10b3602e3701b7538537455cab10d8c878
Sabir <12734@holbertonstudents.com>
2026-02-01 23:19:21 +0400

    Updated

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 
 const massiv = process.argv.slice(2);
 if (massiv === undefined) {
-  console.log(1)
-  process.exit(0)
+  console.log(1);
+  process.exit(0);
 }
 massiv.sort((a, b) => b - a);
 console.log(massiv[1]);
