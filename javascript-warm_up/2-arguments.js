4c0dae2ce2042f9e1809029f81d22afc00fcd4c1
Sabir <12734@holbertonstudents.com>
2026-02-01 22:06:55 +0400

    Updated

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const massivOfArguments = process.argv;
 
-if (massivOfArguments.length == 2) {
+if (massivOfArguments.length === 2) {
   console.log('No argument');
-} else if (massivOfArguments.length == 3) {
+} else if (massivOfArguments.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
